3bb8b73c6b3831f1719068608845296c42efaac5
Eric Kenney <ekenney5@gmail.com>
2014-04-29 11:54:26 -0700

    Working ControllerBase and Session classes

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -9,29 +9,50 @@ class ControllerBase
 
   # setup the controller
   def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # populate the response with content
   # set the responses content type to the given type
   # later raise an error if the developer tries to double render
   def render_content(content, type)
+    raise "Already rendered" if @already_built_response
+    @res.content_type = type
+    @res.body = content
+    session.store_session(@res)
+    @already_built_response = true
   end
 
   # helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # set the response status code and header
   def redirect_to(url)
+    raise "Already rendered" if @already_built_response
+    @res.status = 302
+    @res["Location"] = url
+    session.store_session(@res)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.name.underscore
+    input = File.read("./views/#{controller_name}/#{template_name}.html.erb")
+    # puts "./views/#{controller_name}/#{template_name}.html.erb"
+    b = binding
+    template = ERB.new(input).result b
+    render_content(template, 'text/html')
   end
+
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -5,16 +5,26 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    found_cookie = nil
+    req.cookies.each do |cookie|
+      if cookie.name == "_rails_lite_app"
+        found_cookie = cookie
+      end
+    end
+    @session = found_cookie.nil? ? {} : JSON.parse(found_cookie.value)
   end
 
   def [](key)
+    @session[key]
   end
 
   def []=(key, val)
+    @session[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.cookies << WEBrick::Cookie.new('_rails_lite_app', @session.to_json)
   end
 end
